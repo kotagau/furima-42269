ed3dfc42f151fee7814a5f9fa9361424194d546f
kotagau <kotagau0520@gmail.com>
2025-05-01 14:10:30 +0900

    デプロイ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::Base
+before_action :basic_auth
 
-  def index
-    
+ 
+  private
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
   end
-
 end
