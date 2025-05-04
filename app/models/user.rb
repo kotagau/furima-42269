615f8fe88c89044f086fbb4a0301dcc8685af037
kotagau <kotagau0520@gmail.com>
2025-05-04 15:32:11 +0900

    user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname         , presense:true
+  validates :family_name      , presense:true
+  validates :given_name       , presense:true
+  validates :family_name_read , presense:true
+  validates :given_name_read  , presense:true
+  validates :date_of_birth    , presense:true
+  
+
 end
