efcaf8abd19bd0f605f0c3325c3821ad0634473a
kotagau <kotagau0520@gmail.com>
2025-05-10 18:35:22 +0900

    item display

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,7 @@ before_action :authenticate_user!, except:[:index]
 
 
   def index
+    @items=Item.all.order("created_at DESC")
   end
 
   def new
@@ -22,6 +23,6 @@ before_action :authenticate_user!, except:[:index]
   private
 
   def item_params
-    params.require(:item).permit(:image , :price , :product_name , :product_explanation , :product_category_id , :product_status_id , :deliver_fee_payment_id , :prefecture_id , :deliver_day_id ,).merge(user_id: current_user.id)
+    params.require(:image).permit(:image , :price , :product_name , :product_explanation , :product_category_id , :product_status_id , :deliver_fee_payment_id , :prefecture_id , :deliver_day_id ,).merge(user_id: current_user.id)
   end
 end
