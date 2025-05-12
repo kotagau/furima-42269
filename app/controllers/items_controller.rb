c274ee68887bbd277c075705f85dbb7a43ff021d
kotagau <kotagau0520@gmail.com>
2025-05-12 14:25:35 +0900

    item-delete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   
 before_action :authenticate_user!, except:[:index,:show]
-before_action :item_find,only:[:show,:edit,:update]
+before_action :item_find,only:[:show,:edit,:update,:destroy]
 
   def index
     @items=Item.all.order("created_at DESC")
@@ -45,12 +45,10 @@ before_action :item_find,only:[:show,:edit,:update]
   end
 
 
-  # def delete
-  #   @item = Item.find(params[:id])
-  #   @item.destroy
-  #   redirect_to root_path
-  # end
-
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
 
 
   private
@@ -62,6 +60,5 @@ before_action :item_find,only:[:show,:edit,:update]
   def item_find
     @item=Item.find(params[:id])
   end
-
 
 end
