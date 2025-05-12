2e00060241166397c5917699325ef9dd62c267ba
kotagau <kotagau0520@gmail.com>
2025-05-12 15:06:32 +0900

    item-delete

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -46,8 +46,14 @@ before_action :item_find,only:[:show,:edit,:update,:destroy]
 
 
   def destroy
-    @item.destroy
-    redirect_to root_path
+    if user user_signed_in?
+      if current_user.id == @item.user.id
+          @item.destroy
+          redirect_to root_path
+      else
+         redirect_to root_path
+      end
+    end
   end
 
 
