0bb954a92a68889812f40d4e795494d2049eb47e
kotagau <kotagau0520@gmail.com>
2025-05-15 17:13:24 +0900

    item

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_05_12_085156) do
-  create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+ActiveRecord::Schema[7.1].define(version: 2025_05_15_080627) do
+  create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
@@ -21,7 +21,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_12_085156) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "active_storage_blobs", charset: "utf8mb3", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -33,13 +33,13 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_12_085156) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "active_storage_variant_records", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "active_storage_variant_records", charset: "utf8mb3", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "bought_items", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "bought_items", charset: "utf8mb3", force: :cascade do |t|
     t.bigint "item_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_12_085156) do
     t.index ["user_id"], name: "index_bought_items_on_user_id"
   end
 
-  create_table "deliver_informations", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "deliver_informations", charset: "utf8mb3", force: :cascade do |t|
     t.integer "prefecture_id", null: false
     t.string "post_code", null: false
     t.string "address", null: false
@@ -61,7 +61,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_12_085156) do
     t.index ["bought_item_id"], name: "index_deliver_informations_on_bought_item_id"
   end
 
-  create_table "items", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "items", charset: "utf8mb3", force: :cascade do |t|
     t.integer "price", null: false
     t.bigint "user_id"
     t.string "product_name", null: false
@@ -76,7 +76,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_12_085156) do
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
-  create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+  create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "family_name", null: false
     t.string "given_name", null: false
@@ -85,6 +85,11 @@ ActiveRecord::Schema[7.1].define(version: 2025_05_12_085156) do
     t.date "date_of_birth", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.integer "deliver_day_id", null: false
+    t.integer "deliver_fee_payment_id", null: false
+    t.integer "prefecture_id", null: false
+    t.integer "product_category_id", null: false
+    t.integer "product_status_id", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
